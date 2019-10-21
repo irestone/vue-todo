f66091bd881a9493e511e53f8c5bad1128b549a7
irestone <irestone.work@gmail.com>
2019-10-21 09:40:58 +0300

    Add NewTask form

---
+++ app/views/src/components/forms/NewTask.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-form @submit.prevent="submit">
+    <v-text-field
+      v-model="text"
+      placeholder="New task..."
+      autofocus
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'NewTask',
+  data: () => ({
+    text: '',
+  }),
+  methods: {
+    submit() {
+      if (!this.text) return
+      this.createTask(this.text)
+      this.text = ''
+    },
+    ...mapActions(['createTask']),
+  },
+}
+</script>
