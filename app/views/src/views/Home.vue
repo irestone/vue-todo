a1cf1e87b0d55e542b7328e222a4c7eef55b0f43
irestone <irestone.work@gmail.com>
2019-10-21 09:39:27 +0300

    Add layout

--- app/views/src/views/Home.vue
+++ app/views/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NewTask />
+    <TaskList />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NewTask from '@/components/forms/NewTask'
+import TaskList from '@/components/TaskList'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    NewTask,
+    TaskList,
   },
 }
 </script>
