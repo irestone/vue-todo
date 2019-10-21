2d805b1a60fd95ed4259edfff4142a990a00a49a
irestone <irestone.work@gmail.com>
2019-10-21 13:57:59 +0300

    Add UI state to store

--- app/views/src/store/index.js
+++ app/views/src/store/index.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import { tasks } from './modules/tasks'
+import { ui } from './modules/ui'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    tasks,
-  },
+  modules: { tasks, ui },
 })
---
+++ app/views/src/store/modules/ui.js
@@ -0,0 +1,24 @@
+export const ui = {
+  state: {
+    sortingOrders: {
+      date: [['newest', 'Newest'], ['oldest', 'Oldest']],
+      completed: [
+        ['show', 'Show'],
+        ['show_first', 'Show first'],
+        ['show_last', 'Show last'],
+        ['hide', 'Hide'],
+      ],
+    },
+    sorting: { date: 'newest', completed: 'show' },
+  },
+  mutations: {
+    setSorting(state, order) {
+      state.sorting = { ...state.sorting, ...order }
+    },
+  },
+  actions: {
+    sortBy({ commit }, order) {
+      commit('setSorting', order)
+    },
+  },
+}
